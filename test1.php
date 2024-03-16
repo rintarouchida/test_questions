3cea32e7afb45e504ded2372e9cdc67792ad187c
rintarouchida <ww.stamford@gmail.com>
2024-03-16 19:12:36 +0900

    fix_variable_name

--- test1.php
+++ test1.php
@@ -4,9 +4,9 @@ ini_set("memory_limit", -1);
 
 function main($input) {
   //ゴールまでの残りマス
-  $rest_trout = $input - 1;
+  $rest_squares = $input - 1;
   //ゴールまでサイコロの目が全部6だった場合にサイコロをふる回数
-  $daice_shake_num = ceil($rest_trout / 6);
+  $daice_shake_num = ceil($rest_squares / 6);
   print($daice_shake_num."\n");
 }
 
